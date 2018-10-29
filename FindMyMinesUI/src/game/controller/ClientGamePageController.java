491268befdee7f27d132cc7d0d3f78fe6cee7181
punyapapoonapanont <punyapapoonapanont@10.202.168.143>
2018-10-29 14:49:30 +0500

    fix constantly changing player when timeout

--- FindMyMinesUI/src/game/controller/ClientGamePageController.java
+++ FindMyMinesUI/src/game/controller/ClientGamePageController.java
@@ -420,6 +420,8 @@ public class ClientGamePageController implements Initializable {
 		//playing
 		@FXML
 		void play(MouseEvent event) throws InterruptedException {
+			//set exit startTimer();
+			condition = true;
 			//set color of player to know whose turn is next
 			colorChange();
 			//timer
@@ -431,7 +433,6 @@ public class ClientGamePageController implements Initializable {
 				((Button) event.getTarget()).setStyle("-fx-background-color:#cccccc");
 				((Button) event.getTarget()).setDisable(true);
 				player++;
-
 			}
 
 			if (y.getStyle() == "-fx-font-size: 0.1") {// bomb
@@ -445,72 +446,55 @@ public class ClientGamePageController implements Initializable {
 				setOfScore[player].setText(score + "");
 				keeptrack.put(player, score);
 				player++;
-			
 			}
-			
 			if (player == numOfPlayer) {
 				player = 0;
 			}
 			
+			
 		}
 
-
-	// to display count down from 10 to 0
-	void startTimer() {
-		Task<Void> task = new Task<Void>() {
-			@Override
-			public Void call() throws InterruptedException {
-
-				updateMessage("10");
-				Thread.sleep(1000);
-
-				updateMessage("9");
-				Thread.sleep(1000);
-
-				updateMessage("8");
-				Thread.sleep(1000);
-
-				updateMessage("7");
-				Thread.sleep(1000);
-
-				updateMessage("6");
-				Thread.sleep(1000);
-
-				updateMessage("5");
-				Thread.sleep(1000);
-
-				updateMessage("4");
-				Thread.sleep(1000);
-
-				updateMessage("3");
-				Thread.sleep(1000);
-
-				updateMessage("2");
-				Thread.sleep(1000);
-
-				updateMessage("1");
-				Thread.sleep(1000);
-
-				return null;
-			}
-		};
-		showTime.textProperty().bind(task.messageProperty());
-
-		task.setOnSucceeded(e -> {
-			showTime.textProperty().unbind();
-			showTime.setText("0");
-			//skip this player when timeout
-	        player++;
-	        if (player == numOfPlayer) {
-				player = 0;
-			} 
-	        colorChange();
-		});
-
-		Thread thread = new Thread(task);
-		thread.setDaemon(true);
-		thread.start();
-	}
+		boolean condition = false;
+		//to display count down from 10 to 0
+		void startTimer() {
+			Task <Void> task = new Task<Void>() {
+		        @Override public Void call() throws InterruptedException {
+		        		for (int i = 10; i>=0; i--) {
+		        			//exit startTimer();
+		        			if (condition == true) {
+		        				return null;
+		                }
+		        			updateMessage(i+"");
+		        			Thread.sleep(1000);
+		        		}
+		          return null;
+		        }
+		      };
+		    //exit startTimer();
+		      if(condition == true) {
+		    	  	condition = false;
+		    	  	return;
+		      }
+		      showTime.textProperty().bind(task.messageProperty());
+		      task.setOnSucceeded(e -> {
+		    	  	showTime.textProperty().unbind();
+		        showTime.setText("0");
+		        //skip this player when timeout
+		        player++;
+		        if (player == numOfPlayer) {
+					player = 0;
+				} 
+		        colorChange();
+		      	//set condition back to default
+		        condition = false;
+		        //startTimer after timeout
+		        startTimer();
+		      });
+		   
+		      Thread thread = new Thread(task);
+		      thread.setDaemon(true);
+		      thread.start();
+		    }
 
 	Integer[] nameOfPlayer = new Integer[10];
 	private static Map<Integer, Integer> sorted = new Hashtable<Integer,Integer>();
