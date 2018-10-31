10dc69cc4ba8319cae098ca82de3dc1a87397c82
queenievatcha <boonsita.queen@gmail.com>
2018-11-01 00:31:17 +0700

    removed all bomb logics from client start page

--- FindMyMinesUI/src/game/controller/ClientStartPageController.java
+++ FindMyMinesUI/src/game/controller/ClientStartPageController.java
@@ -49,126 +49,25 @@ public class ClientStartPageController {
 
 	@FXML
 	void start(ActionEvent event) throws IOException {
-		// assign bomb to the slot
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < 6; j++) {
-
-				float result = (float) Math.random();
-				if (result < 0.5) {
-					result = 0;
-				}
-				if (result >= 0.5) {
-					result = 1;
-				}
-				if (numBomb >= 11) {
-					result = 0;
-				}
-				if (result == 0) {
-					valueOfSpace[i][j] = 0;// free space
-				}
-				if (result == 1) {
-					valueOfSpace[i][j] = 1;// bomb
-					numBomb++;
-				}
-
-			}
-		}
-
-		// fix number of bomb to 11
-		while (numBomb != 11) {
-			for (int i = 0; i < 6; i++) {
-				for (int j = 0; j < 6; j++) {
-
-					if (valueOfSpace[i][j] == 0) {
-						int result = (int) Math.random();
-						if (result < 0.5) {
-							result = 0;
-						}
-						if (result >= 0.5) {
-							result = 1;
-						}
-						if (result == 0) {
-							valueOfSpace[i][j] = 0;// free space
-						}
-						if (result == 1) {
-							valueOfSpace[i][j] = 1;// bomb
-							numBomb++;
-						}
-					}
-				}
-
-			}
-		}
-
-		// set number in free slot
-
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < 6; j++) {
-
-				int countBombAround = 0;
-				if (valueOfSpace[i][j] == 0) { // if this is free slot
-					if (i - 1 >= 0 && j - 1 >= 0) { // if there is a slot
-						if (valueOfSpace[i - 1][j - 1] == 1) { // if the upperleft is bomb
-							countBombAround++;
-						}
-					}
-					if (i >= 0 && j - 1 >= 0) {
-						if (valueOfSpace[i][j - 1] == 1) {
-							countBombAround++;
-						}
-					}
-					if (i + 1 <= 5 && j - 1 >= 0) {
-						if (valueOfSpace[i + 1][j - 1] == 1) {
-							countBombAround++;
-						}
-					}
-					if (i - 1 >= 0 && j >= 0) {
-						if (valueOfSpace[i - 1][j] == 1) {
-							countBombAround++;
-						}
-					}
-					if (i + 1 <= 5 && j >= 0) {
-						if (valueOfSpace[i + 1][j] == 1) {
-							countBombAround++;
-						}
-					}
-					if (i - 1 >= 0 && j + 1 <= 5) {
-						if (valueOfSpace[i - 1][j + 1] == 1) {
-							countBombAround++;
-						}
-					}
-					if (i >= 0 && j + 1 <= 5) {
-						if (valueOfSpace[i][j + 1] == 1) {
-							countBombAround++;
-						}
-					}
-					if (i + 1 <= 5 && j + 1 <= 5) {
-						if (valueOfSpace[i + 1][j + 1] == 1) {
-							countBombAround++;
-						}
-					}
-					bombAround[i][j] = countBombAround;
-
-				}
-			}
-		}
-		server = txtServer.getText();
-		userName = txtUser.getText();
+		
+		server = txtServer.getText().trim();
+		userName = txtUser.getText().trim();
+		
 		if (server.isEmpty() == true && userName.isEmpty() == true) {
 			warnIP.setVisible(true);
 			warnName.setVisible(true);
 			return;
 		}
-		if (server.isEmpty() == true) {
+		else if (server.isEmpty() == true) {
 			warnIP.setVisible(true);
 			warnName.setVisible(false);
 			return;
 		}
-		if (userName.isEmpty() == true) {
+		else if (userName.isEmpty() == true) {
 			warnName.setVisible(true);
 			warnIP.setVisible(false);
 			return;
-		}
+		}		
 
 		AnchorPane gamePage = (AnchorPane) FXMLLoader.load(getClass().getResource("/game/view/ClientGamePage.fxml"));
 		Scene scene = new Scene(gamePage);
@@ -180,18 +79,4 @@ public class ClientStartPageController {
 
 	}
 
-	public static int getValueOfSpace(int i, int j) {
-		int valueofspace = valueOfSpace[i][j];
-		return valueofspace;
-
-	}
-
-	public static int getNumBombAround(int i, int j) {
-		int numbombaround = bombAround[i][j];
-		return numbombaround;
-	}
-	public String getName() {
-		return txtUser.getText();
-		
-	}
 }
